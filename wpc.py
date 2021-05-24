911a52ee9f68fd660b07f49c37a491198e48c915
Anci <kanariosun@gmail.com>
2021-05-24 11:20:03 +0400

    Update wpc.py
    add CYCLE82 to CYCLE81 replacer

--- wpc.py
+++ wpc.py
@@ -20,11 +20,27 @@ if typeWpc == '"CYLINDER"':
     # диаметр заготовки
     diam = x[8][0:-1]
     
-    stringWPC = 'N5 G17\nN10 WORKPIECE(,””,,' + typeWpc + ',64,0,-' + height +',' + diam + '.' + diam +')'
+    stringWPC = 'N5 G17\nN10 WORKPIECE(,"",,' + typeWpc + ',64,0,-' + height +',' + diam + '.' + diam +')'
+    
+    f = open('wpc.txt', 'r')
+    yoba = f.readlines()
+    f = open('wpc.txt', 'w')
+    for line in yoba:
+        if 'SUPA' not in line:
+            f.write(re.sub(r'N10\s\S+', stringWPC, line))
+    f.close()
     
 elif typeWpc == '"BOX"':
 
-    stringWPC = 'N5 G17\nN10 WORKPIECE(,””,,' + typeWpc + ',112,0,-3,3,0,0,100,200)'
+    stringWPC = 'N5 G17\nN10 WORKPIECE(,"",,' + typeWpc + ',112,0,-3,3,0,0,100,200)'
+    
+    f = open('wpc.txt', 'r')
+    yoba = f.readlines()
+    f = open('wpc.txt', 'w')
+    for line in yoba:
+        if 'SUPA' not in line:
+            f.write(re.sub(r'N10\s\S+', stringWPC, line))
+    f.close()
     
 else:
 
@@ -32,11 +48,12 @@ else:
     
 #stringWPC - переменная, на которую нужно будет заменять найденную строку с заготовкой при записи
 
+centerDrilling = 'MCALL CYCLE81(100,0,2,-0.5,,0,1,12) ' #цикл центровки
+
 f = open('wpc.txt', 'r')
 yoba = f.readlines()
 f = open('wpc.txt', 'w')
 for line in yoba:
     if 'SUPA' not in line:
-        f.write(re.sub(r'N\S+', r'huy', line))
-
+        f.write(re.sub(r'\S+.MCALL.CYCLE82\S+', centerDrilling, line))
 f.close()
